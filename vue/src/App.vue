5fccb81e0be8ca7da4ff312947f75a668622934b
ao-anam <alex@anam.ai>
2024-09-23 22:16:17 +0100

    feat: vue example

---
+++ vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PersonaPlayer from './components/PersonaPlayer.vue'
+</script>
+
+<template>
+  <main class="min-h-screen flex items-center justify-center">
+    <PersonaPlayer />
+  </main>
+</template>
+
